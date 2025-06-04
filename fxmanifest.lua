7cf4b9088068bab2313b5355c7e4b66cca38cdc5
Mac <145077500+MacDevelopment@users.noreply.github.com>
2025-06-04 18:35:49 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mac Development'
+description 'Plays /e map when the pause menu opens, and stops it when you close the menu.'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
